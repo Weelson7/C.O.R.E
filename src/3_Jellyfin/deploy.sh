c588a40a9521c202b3902faa33f48c6ccbc8220c
Weelson7 <william.adam@student.kuleuven.be>
2026-03-29 16:37:02 +0200

    Feixed jellyfin deploy 4

--- src/3_Jellyfin/deploy.sh
+++ src/3_Jellyfin/deploy.sh
@@ -193,9 +193,16 @@ server {
         proxy_set_header   X-Real-IP         \$remote_addr;
         proxy_set_header   X-Forwarded-For   \$proxy_add_x_forwarded_for;
         proxy_set_header   X-Forwarded-Proto \$scheme;
-        proxy_set_header   Upgrade           \$http_upgrade;
-        proxy_set_header   Connection        "upgrade";
+        proxy_set_header   X-Forwarded-Host  \$host;
+        proxy_set_header   X-Forwarded-Port  \$server_port;
+        
+        proxy_buffering          off;
+        proxy_cache              off;
+        proxy_redirect           off;
+        
         proxy_read_timeout 3600;
+        proxy_send_timeout 3600;
+        proxy_connect_timeout 600;
     }
 
     add_header X-Frame-Options        "SAMEORIGIN" always;
